b2d32633429ba5b09d2a42b2bdf17ef9e27d6764
Federico Maleh <fedemaleh@yahoo.com.ar>
2026-03-30 09:06:44 -0300

    Improve terra example (#6)
    * Improve terra example
    
    * Use latest tofu module
    
    * Fix tofu modules usage

--- static-files/specs/terraform/main.tf
+++ static-files/specs/terraform/main.tf
@@ -1,10 +1,10 @@
 locals {
   scope_definition = {
-    git_repo       = "nullplatform/scopes-static-files"
-    git_ref        = "main"
-    git_scope_path = "static-files"
-    name           = "Static Files"
-    description    = "Allows you to deploy static files applications"
+    repository_service_spec        = "nullplatform/scopes-static-files"
+    repository_service_spec_branch = "main"
+    service_path                   = "static-files"
+    name                           = "Static Files"
+    description                    = "Allows you to deploy static files applications"
     actions = [
       "create-scope",
       "delete-scope",
@@ -15,21 +15,28 @@ locals {
       "delete-deployment",
     ]
   }
+
+  repository_base_url = "https://${var.github_token}@raw.githubusercontent.com/${local.scope_definition.repository_service_spec}/refs/heads"
 }
 
 module "scope_definition" {
-  source = "git::https://github.com/nullplatform/main-terraform-modules.git//modules/nullplatform/scope-definition?ref=main"
+  source = "git::https://github.com/nullplatform/tofu-modules.git//nullplatform/scope_definition?ref=feature/remove-org-nrn"
 
   nrn        = var.nrn
   np_api_key = var.np_api_key
 
-  github_repo_url   = "https://${var.github_token}@github.com/${local.scope_definition.git_repo}"
-  github_ref        = local.scope_definition.git_ref
-  github_scope_path = local.scope_definition.git_scope_path
-  scope_name        = local.scope_definition.name
-  scope_description = local.scope_definition.description
+  repository_service_spec            = local.repository_base_url
+  repository_service_spec_branch     = local.scope_definition.repository_service_spec_branch
+  repository_scope_template          = local.repository_base_url
+  repository_scope_template_branch   = local.scope_definition.repository_service_spec_branch
+  repository_action_templates        = local.repository_base_url
+  repository_action_templates_branch = local.scope_definition.repository_service_spec_branch
+  service_path                       = local.scope_definition.service_path
+  service_spec_name                  = local.scope_definition.name
+  service_spec_description           = local.scope_definition.description
 
-  action_spec_names = local.scope_definition.actions
+  action_spec_names          = local.scope_definition.actions
+  create_scope_configuration = true
 }
 
 module "scope_definition_agent_association" {
@@ -38,11 +45,22 @@ module "scope_definition_agent_association" {
   api_key                  = var.np_api_key
   nrn                      = var.nrn
   scope_specification_id   = module.scope_definition.service_specification_id
-  scope_specification_slug = module.scope_definition.service_specification_slug
+  scope_specification_slug = module.scope_definition.service_slug
   tags_selectors           = var.tags
 
-  repository_notification_channel        = "https://${var.github_token}@raw.githubusercontent.com/${local.scope_definition.git_repo}/refs/heads"
-  repository_notification_channel_branch = local.scope_definition.git_ref
-  service_path                           = local.scope_definition.git_scope_path
-  repo_path                              = "/root/.np/${local.scope_definition.git_repo}"
+  repository_notification_channel        = "https://${var.github_token}@raw.githubusercontent.com/${local.scope_definition.repository_service_spec}/refs/heads"
+  repository_notification_channel_branch = local.scope_definition.repository_service_spec_branch
+  service_path                           = local.scope_definition.service_path
+  repo_path                              = "/root/.np/${local.scope_definition.repository_service_spec}"
+}
+
+resource "nullplatform_provider_config" "static_files_configuration" {
+  nrn = var.nrn
+
+  type       = module.scope_definition.provider_specification_id
+  dimensions = {}
+
+  attributes = jsonencode({
+    region = "us-east-1"
+  })
 }
